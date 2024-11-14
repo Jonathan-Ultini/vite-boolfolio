4918f639f7ea7dd081819203a03a2fc707b8209d
Jonathan-Ultini <jonathanultini@gmail.com>
2024-11-14 14:35:36 +0100

    aggiunta hiamata axios e visualizazione in pagina tramite componente

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="project-card">
+    <h3>{{ project.name }}</h3>
+    <a href="{{ project.link }}">{{ project.link }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: Object
+  }
+};
+</script>
