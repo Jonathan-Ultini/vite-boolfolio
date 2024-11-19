684bce363bd2d745943f16be5aaef45daea66ca0
Jonathan-Ultini <jonathanultini@gmail.com>
2024-11-19 16:06:20 +0100

    fix homepage e collegamento axios

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,50 +1,52 @@
 <template>
   <div>
-    <h1 class="text-center mb-4">Elenco Progetti</h1>
-    <div class="table-responsive">
-      <table class="table table-striped">
-        <thead class="table-dark">
-          <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Link</th>
-            <th>Azioni</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="project in projects" :key="project.id">
-            <td>{{ project.id }}</td>
-            <td>{{ project.name }}</td>
-            <td><a href="{{ project.link }}">{{ project.link }}</a></td>
-            <td>
-              <router-link :to="{ name: 'project-detail', params: { id: project.id } }" class="btn btn-info btn-sm">
-                Dettagli
-              </router-link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <h1>Progetti</h1>
+    <router-link to="/projects/create" class="btn btn-primary">Crea Nuovo Progetto</router-link>
+    <table class="table mt-3">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Link</th>
+          <th>Azioni</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>{{ project.name }}</td>
+          <td><a :href="project.link" target="_blank">{{ project.link }}</a></td>
+          <td>
+            <router-link :to="`/projects/${project.id}/edit`" class="btn btn-warning btn-sm">Modifica</router-link>
+            <button @click="deleteProject(project.id)" class="btn btn-danger btn-sm">Elimina</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'Projects',
   data() {
     return {
-      projects: []
+      projects: [],
     };
   },
-  mounted() {
-    axios.get('http://127.0.0.1:8000/api/projects')
-      .then(response => {
-        this.projects = response.data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  created() {
+    this.fetchProjects();
+  },
+  methods: {
+    async fetchProjects() {
+      const response = await axios.get("http://127.0.0.1:8000/api/projects");
+      this.projects = response.data;
+    },
+    async deleteProject(id) {
+      if (confirm("Sei sicuro di voler eliminare questo progetto?")) {
+        await axios.delete(`http://127.0.0.1:8000/api/projects/${id}`);
+        this.fetchProjects();
+      }
+    },
+  },
 };
 </script>
