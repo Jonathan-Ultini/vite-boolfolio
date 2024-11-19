5fbb50374a36800ec6035faad2f11715d7399a66
Jonathan-Ultini <jonathanultini@gmail.com>
2024-11-19 16:02:39 +0100

    modifica route per aggiunte crud

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import Projects from '../pages/Projects.vue';
 import ProjectDetail from '../pages/ProjectDetail.vue';
+import ProjectCreate from '../pages/ProjectCreate.vue';
+import ProjectEdit from '../pages/ProjectEdit.vue';
 import TypeList from "../pages/TypeList.vue";
 import TechnologyList from "../pages/TechnologyList.vue";
 
@@ -9,9 +11,12 @@ const routes = [
   { path: "/", name: "home", component: Home },
   { path: "/projects", name: "projects", component: Projects },
   { path: '/projects/:id', name: 'project-detail', component: ProjectDetail, props: true },
+  { path: "/projects/create", name: "project-create", component: ProjectCreate },
+  { path: "/projects/:id/edit", name: "project-edit", component: ProjectEdit },
   { path: "/types", name: "types", component: TypeList },
   { path: "/technologies", name: "technologies", component: TechnologyList },
 ];
+
 const router = createRouter({
   history: createWebHistory(),
   routes
